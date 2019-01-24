2d97326962d9a07ed56515283e4fb3d5a4d6c207
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-24 01:41:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,8 @@ def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
+#my initial work:
+#
 #def reverse_each_word (string)
 #  array = string.split
 #  array.collect do |x|
