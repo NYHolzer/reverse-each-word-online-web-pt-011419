c16e49ad38e63b27067900b3edd25d163e2b833d
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-24 01:41:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
-end
+#def reverse_each_word(sentence)
+#  sentence.split.collect {|word| word.reverse}.join(" ")
+# end
 
 #my initial work:
 #
-#def reverse_each_word (string)
+def reverse_each_word (string)
 #  array = string.split
-#  array.collect do |x|
+#  array.each do |x|
 #    x.reverse!
 #  end
 #  array.join " "
